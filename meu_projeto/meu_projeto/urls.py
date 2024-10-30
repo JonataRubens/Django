c276f1b10ef273dba012be598053f50268fae83b
WEB <jonataR75@gmail.com>
2024-10-29 23:03:42 -0300

    up

--- meu_projeto/meu_projeto/urls.py
+++ meu_projeto/meu_projeto/urls.py
@@ -1,13 +1,13 @@
 from . import views
 from django.contrib import admin
 from django.urls import path 
-from .views import CarroUpdateView, ListarCarros, adicionar_carro, exclusao_carro
+from .views import CarroUpdateView, ListarCarros, adicionar_carro, exclusao_carro, login_view
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', ListarCarros.as_view(), name='/login/'),
+    path('', login_view, name='/login/'),
     path('carros/', ListarCarros.as_view(), name='listar_carros'),
     path('<int:pk>/', CarroUpdateView.as_view(), name='editar_carro'),
     path('carro/<int:carro_id>/excluir/', exclusao_carro, name='excluir_carro'),
