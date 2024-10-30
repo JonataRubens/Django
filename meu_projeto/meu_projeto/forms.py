7ef0b488c326a0eb9dbbe3d9cff5b5663b6155a3
WEB <jonataR75@gmail.com>
2024-10-29 23:25:20 -0300

    del

--- meu_projeto/meu_projeto/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Carro
-
-class CarroForm(forms.ModelForm):
-    class Meta:
-        model = Carro
-        fields = ['montadora', 'modelo', 'ano', 'cor', 'imagem']
